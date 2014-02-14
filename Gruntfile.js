acc3ec9e59e7321750dc790994669adbeab80a64
Andy Smith <asmith.esq@gmail.com>
2014-02-14 16:12:39 -0600

    Add sass and watch to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,20 @@ module.exports = function(grunt) {
 
   // Project configuration
   grunt.initConfig({
-
+    sass: {
+      dist: {
+        files: {
+          "assets/stylesheets/styles.min.css" : "assets/stylesheets/source/styles.scss"
+        },
+        options: {
+          style: "compressed",
+          sourceMap: false
+        }
+      }
+    },
   })
 
   // Load tasks
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks('grunt-contrib-watch');
 };
